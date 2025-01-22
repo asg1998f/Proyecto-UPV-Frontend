b990cd8e2b333dd512f32480edc9ff265fec9cff
fullstackLuis91 <luis_rv91@hotmail.com>
2025-01-22 12:06:29 +0100

    header hecho

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Landing from './components/Landing/Landing'
 import InformeRegistro from './components/InformeRegistro/InformeRegistro'
+import Header from './components/Header/Header'
 
 
 function App() {
@@ -10,8 +11,8 @@ function App() {
   return (
     <>
     <BrowserRouter>
+    <Header/>
     <Routes>
-
       <Route path="/" element={ <Landing/>} />
       <Route path="/informeregistro" element={<InformeRegistro/>}/>
     </Routes>
