1faa5c0272c4a99ea7cd39ea8effd4e6c6e2c42a
mario <daw@grupapunts.es>
2025-01-22 10:11:52 +0100

    Landing first

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import './App.css'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import './App.css'
+import Landing from './components/Landing/Landing'
 
 function App() {
-  <>
-  </>
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+
+      <Route path="/" element={ <Landing/>} />
+
+    </Routes>
+    </BrowserRouter>
+    
+    </>
+  )
 }
 
 export default App
