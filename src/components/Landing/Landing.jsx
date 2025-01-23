22b82ea666ecbbbd3377227f020b884c7017a6b0
unknown <joan_mart_her@hotmail.com>
2025-01-23 16:25:35 +0100

    Añadir boton para seleccionar y subir imagenes

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
+import { useEffect, useState } from 'react'
+import { Button } from "antd";
+import { useDispatch, useSelector } from "react-redux";
 
 const Landing = () => {
-  const subirFotos = () => {
+  const [files, setFiles] = useState([]);
 
+  const subirArchivos = (file) =>{
+    console.log(file);
+    
   }
+
+  const handleFileChange = (event) => {
+    setFiles(event.target.files);
+  };
+  
+  const handleUpload = async () => {
+    for (const file of files) {
+      await subirArchivos(file);
+    }
+  };
 
   return (
     <div>
-      <h1>Subir Archivos a Google Drive</h1>
-      <input
-        type="file"
-        multiple
-        onChange={handleFileChange}
-        accept="*/*"
-      />
-      <button onClick={subirFotos} disabled={files.length === 0}>
-        Subir Archivos
-      </button>
+      <input type="file" id="fileInput" multiple onChange={handleFileChange}
+        accept="*/*"/>
+      <Button onClick={subirArchivos} disabled={files.length === 0}>Subir foto</Button>
     </div>
-  );
-};
+  )
+}
 
 export default Landing
