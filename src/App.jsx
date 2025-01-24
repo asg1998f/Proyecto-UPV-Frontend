593235a31898ec7d3b532bc8e2ec4dfe7b68cdaf
mario <daw@grupapunts.es>
2025-01-24 18:17:14 +0100

    header y dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Landing from './components/Landing/Landing'
 import InformeRegistro from './components/InformeRegistro/InformeRegistro'
 import Header from './components/Header/Header'
 import Login from './components/Login/Login'
+import Header from './components/HeaderM/HeaderM'
 
 
 function App() {
@@ -16,9 +17,20 @@ function App() {
     <Routes>
       <Route path="/" element={ <Landing/>} />
       <Route path="/informeregistro" element={<InformeRegistro/>}/>
+
       <Route path="/login" element={<Login/>}/>
     </Routes>
     </BrowserRouter>
+    <div>
+      <FaseSecado
+      title = "Secado"
+      logo = {avion}
+      />
+      <FaseLimpieza 
+      title = "Limpieza"
+      logo = {escoba}
+      /> 
+    </div>
     
     </>
   )
