9a64661c5dc386a9255c209187f81a0191d17e36
unknown <joan_mart_her@hotmail.com>
2025-01-24 20:20:29 +0100

    Añadir vista Home Distributiva y Header para movil

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,36 @@ import './App.css'
 import Landing from './components/Landing/Landing'
 import InformeRegistro from './components/InformeRegistro/InformeRegistro'
 import Header from './components/Header/Header'
+import HomeDistributiva from './components/HomeDistributiva/HomeDistributiva'
+import avion from './assets/iconos/LogoSalvar.png'
 
+const esMobil = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+  navigator.userAgent
+)
 
 function App() {
 
-  return (
+  return (   
     <>
+    {esMobil ? 
+    (      
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={ <Landing/>} />
-      <Route path="/informeregistro" element={<InformeRegistro/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/home" element={<HomeDistributiva
+      logo = {avion}/>}></Route>
+      </Routes>
     </BrowserRouter>
-    
+      ) 
+    :
+     (
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={ <Landing/>} />
+        <Route path="/informeregistro" element={<InformeRegistro/>}/>
+      </Routes>
+      </BrowserRouter>)}
+
     </>
   )
 }
