a0b50e2bf42ce5e90971d6387ff872a525771c8e
xavi <antunez.xavi.grao@gmail.com>
2025-01-25 13:55:14 +0100

    Dashboard basico

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Landing from './components/Landing/Landing'
 import InformeRegistro from './components/InformeRegistro/InformeRegistro'
 import Header from './components/Header/Header'
 import Login from './components/Login/Login'
+import Dashboard from './components/Dashboard/Dashboard'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Route path="/" element={ <Landing/>} />
       <Route path="/informeregistro" element={<InformeRegistro/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
     </Routes>
     </BrowserRouter>
     
