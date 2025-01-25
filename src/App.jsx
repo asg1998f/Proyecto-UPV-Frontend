8380b192f43061cc20d58bc25e612b1579c1c413
mario <daw@grupapunts.es>
2025-01-25 11:11:14 +0100

    cambios extra

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import Landing from './components/Landing/Landing'
 import InformeRegistro from './components/InformeRegistro/InformeRegistro'
 import Header from './components/Header/Header'
 import Login from './components/Login/Login'
-<<<<<<< HEAD
-import Header from './components/HeaderM/HeaderM'
-=======
-import HomeDistributiva from './components/HomeDistributiva/HomeDistributiva'
-import LogoSalvar from './assets/iconos/LogoSalvar.png'
->>>>>>> develop
+import HeaderM from './components/HeaderM/HeaderM'
 
 const esMobil = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
   navigator.userAgent
@@ -18,32 +13,57 @@ const esMobil = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
 
 function App() {
 
-  return (   
-    <>
-    {/* {esMobil ? 
-    (       */}
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={ <Landing/>} />
-      <Route path="/informeregistro" element={<InformeRegistro/>}/>
 
-      <Route path="/login" element={<Login/>}/>
-    </Routes>
+  return (
+    <>
+    {esMobil ?
+    (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/ubicacionAlmacenmb" element={<UbicacionAlmacen
+      logo = {avion}/>}></Route>
+      </Routes>
     </BrowserRouter>
-    <div>
-      <FaseSecado
-      title = "Secado"
-      logo = {avion}
-      />
-      <FaseLimpieza 
-      title = "Limpieza"
-      logo = {escoba}
-      /> 
-    </div>
-    
+      )
+    :
+     (
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={ <Landing/>} />
+        <Route path="/informeregistro" element={<InformeRegistro/>}/>
+        <Route path="/login" element={<Login/>}/>
+        {/* <Route path="/ubicacionAlmacen" element={<UbicacionAlmacen/>}/> */}
+      </Routes>
+      </BrowserRouter>)}
     </>
   )
 }
+//   return (   
+//     <>
+//     {esMobil ? 
+//     (      
+//     <BrowserRouter>
+//     <Header/>
+//     <Routes>
+//       <Route path="/" element={ <Landing/>} />
+//       <Route path="/informeregistro" element={<InformeRegistro/>}/>
+
+//     </Routes>
+//     </BrowserRouter>
+//     <div>
+//       <FaseSecado
+//       title = "Secado"
+//       logo = {avion}
+//       />
+//       <FaseLimpieza 
+//       title = "Limpieza"
+//       logo = {escoba}
+//       /> 
+//     </div>
+    
+//     </>
+//   )
+// }
 
 export default App
