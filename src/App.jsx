8446b635e9866df69de814bce743b29aae8fce14
Catalina <cblvallada@gmail.com>
2025-01-27 11:35:40 +0100

    HeaderRestauracion modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Restauracion from './components/Restauration/Restauracion'
 import Restauracion2 from './components/Restauracion2';
-import RestauracionCuatro from './components/RestauracionCuatro/RestauracionCuatro';
+import Restauracion4 from './components/RestauracionCuatro/RestauracionCuatro';
 import HomeDistributiva from './components/HomeDistributiva/HomeDistributiva'
+import Landing from './components/Landing/Landing'
+//import InformeRegistro from './components/InformeRegistro/InformeRegistro'
+import Login from './components/Login/Login'
+import Header from './components/Header/Header'
 import LogoSalvar from './assets/iconos/LogoSalvar.png'
 
 const esMobil = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
@@ -21,9 +25,9 @@ function App() {
       <Routes>
         <Route path="/home" element={<HomeDistributiva
       logo = {LogoSalvar}/>}></Route>
-            <Route path="/restauracion" element={<Restauracion/>}/>
+      <Route path="/restauracion" element={<Restauracion/>}/>
       <Route path="/restauracion2" element={<Restauracion2/>}/>
-      <Route path="/restauracionCuatro" element={<RestauracionCuatro/>}/>
+      <Route path="/restauracion4" element={<Restauracion4/>}/>
       </Routes>
     </BrowserRouter>
       ) 
@@ -33,7 +37,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={ <Landing/>} />
-        <Route path="/informeregistro" element={<InformeRegistro/>}/>
+        {/* <Route path="/informeregistro" element={<InformeRegistro/>}/> */}
         <Route path="/login" element={<Login/>}/>
       </Routes>
       </BrowserRouter>)}
--- src/components/HeaderRestauracion/HederRestauracion.jsx
+++ src/components/HeaderRestauracion/HederRestauracion.jsx
@@ -6,22 +6,16 @@ import Broom from "../../assets/images/escobita.png"
 
 const HeaderRestauracion =()=>{
     return (
-      <div className="logo">
-      <img className="vector" src={Vector} />
-      <div className="frame-427319580">
-        <div className="frame-427319550">
+      <div className="logoRestaurar">
+      <img className="vectorRestaurar" src={Vector} />
           <div className="icono">
             <img className="fi-sr-broom" src={Broom} />
           </div>
           <div className="almac-n">Restaurar</div>
-        </div>
         <div className="frame-427319552">
           <div className="_0012-ft">0012-FT</div>
-          <div className="_3-lbumes-40-fotos-y-1-marco">
             3 álbumes, 40 fotos y 1 marco
-          </div>
         </div>
-      </div>
     </div>
         )
 }
