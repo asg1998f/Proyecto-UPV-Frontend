f8ec0808ef6c9627bb3b256be8717daac059a453
unknown <joan_mart_her@hotmail.com>
2025-01-27 16:19:19 +0100

    Añadir componente Validacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
 import './App.css'
 import UbicacionAlmacen from './components/UbicacionAlmacen/UbicacionAlmacen'
 import Header from './components/Header/Header'
 import Login from './components/Login/Login'
 import Restauracion from './components/Restauration/Restauracion'
-import Restauracion2 from './components/Restauracion2';
 import Restauracion4 from './components/RestauracionCuatro/RestauracionCuatro';
 import HomeDistributiva from './components/HomeDistributiva/HomeDistributiva'
 import Landing from './components/Landing/Landing'
@@ -12,7 +12,6 @@ import LogoSalvar from './assets/iconos/LogoSalvar.png'
 import Dashboard from './components/Dashboard/Dashboard'
 import Almacenaje from './components/Almacenaje/Almacenaje'
 import IconoAlmacenH from './assets/iconos/IconoAlmacenH.png'
-
 
 const esMobil = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
   navigator.userAgent
@@ -28,7 +27,6 @@ function App() {
         <Route path="/ubicacionAlmacen" element={<UbicacionAlmacen icono = {IconoAlmacenH} />}></Route>
         <Route path="/almacenaje" element={<Almacenaje icono = {IconoAlmacenH}/>}></Route>
         <Route path="/restauracion" element={<Restauracion/>}/>
-        <Route path="/restauracion2" element={<Restauracion2/>}/>
         <Route path="/restauracion4" element={<Restauracion4/>}/>
       </Routes>
     </BrowserRouter>
