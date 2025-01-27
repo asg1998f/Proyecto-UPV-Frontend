b347461f9ca97e81b06d80c619c9068be1562d93
fullstackLuis91 <luis_rv91@hotmail.com>
2025-01-27 15:52:12 +0100

    pull hecho

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import LoginM from './components/LoginM/LoginM'
 /* import './styles/globalstyles.scss' */
 import Header from './components/Header/Header'
 import Login from './components/Login/Login'
-import Restauracion from './components/Restauration/Restauracion'
-import Restauracion2 from './components/Restauracion2';
+import Restauracion from './components/Restauracion/Restauracion'
 import Restauracion4 from './components/RestauracionCuatro/RestauracionCuatro';
 import HomeDistributiva from './components/HomeDistributiva/HomeDistributiva'
 import Landing from './components/Landing/Landing'
@@ -33,9 +32,7 @@ function App() {
         <Route path="/ubicacionAlmacen" element={<UbicacionAlmacen icono = {IconoAlmacenH} />}></Route>
         <Route path="/almacenaje" element={<Almacenaje icono = {IconoAlmacenH}/>}></Route>
         <Route path="/restauracion" element={<Restauracion/>}/>
-        <Route path="/restauracion2" element={<Restauracion2/>}/>
         <Route path="/restauracion4" element={<Restauracion4/>}/>
-
        <Route path="/restauracionimagenes" element={ <RestauracionImagenes/>} />
 
       </Routes>
