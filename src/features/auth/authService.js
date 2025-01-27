b0410e27d4760da6437a4d3234402563c407b4f8
xavi <antunez.xavi.grao@gmail.com>
2025-01-27 17:44:36 +0100

    gets en auth

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
-
-const API_URL = "http://localhost:3001/users";
+const API_USERS = "http://localhost:3001/users";
+const API_DNI = "http://localhost:3001/DNIVerificados";
 
 const register = async (userData) => {
-  const res = await axios.post(API_URL, userData);
+  const res = await axios.post(API_USERS, userData);
   return res.data;
 };
 
-
 const login = async(userData)=>{
-    const res = await axios.post(API_URL + '/login',userData)
+    const res = await axios.post(API_USERS + '/login',userData)
     if (res.data) {
         localStorage.setItem("user", JSON.stringify(res.data.user));
         localStorage.setItem("token", JSON.stringify(res.data.token));      
@@ -18,11 +17,31 @@ const login = async(userData)=>{
     return res.data
 }
 
+const getUserById = async (id) => {
+  const res = await axios.get(API_USERS + "/id/" + id);
+  return res.data;
+};
+const getDniById = async (id) => {
+  const res = await axios.get(API_DNI + "/id/" + id);
+  return res.data;
+};
 
+const getAllUsers = async () => {
+  const res = await axios.get(API_USERS + "/");
+  return res.data;
+};
+const getAllDni = async () => {
+  const res = await axios.get(API_DNI + "/");
+  return res.data;
+};
 
 const authService = {
   register,
-  login
+  login,
+  getUserById,
+  getDniById,
+  getAllUsers,
+  getAllDni
 };
 
 export default authService;
