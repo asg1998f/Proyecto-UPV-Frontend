f2884d3fc871b8f9ebd027478124485f78390db5
Mario Tomas <mariotomasdaw@gmail.com>
2025-01-28 17:04:51 +0100

    login acabao

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,74 +1,75 @@
 import React from 'react';
 import './Login.scss';
+import rostro from '../../assets/images/rostroDestrozado.jpg';
+import logo from '../../assets/iconos/LogoN.png'
+import flecha from '../../assets/iconos/Flecha.png'
 
 const Login = () => {
   return (
-    <div class="container">
-      <div class="logincontainer">
+    <div class="log-in-desktop">
+      <img
+        className='rostro'
+        class="chris-lee-70-l-1-t-dai-6-r-m-unsplash-2"
+        src={rostro}
+      />
+      <div class="frame-427319587">
+        <div class="frame-427319586">
+          <div class="salvem-les-fotos">
 
-        {/* imagen apartada */}
-        <img
-          class="fotoderecha"
-          src="chris-lee-70-l-1-t-dai-6-r-m-unsplash-20.png"
-        />
+            <span class="salvem-les-fotos-span2">
+              <img
+                className='logo'
+                class=""
+                src={logo}
+              />
+            </span>
 
-        <div class="containerizquierda">
-          <div class="titulo">
-            <div class="salvem-les-fotos">
-              <span>
-                <span class="salvem-les-fotos-span">
-                  SALVEM
-                  <br />
-                </span>
-                <span class="salvem-les-fotos-span2">LES FOTOS</span>
-              </span>
-            </div>
           </div>
-          <div class="divform">
-            <div class="divform2">
-              <div class="userinput">
-                <div class="divuser">
-                  <div class="user">
-                    <div class="usuario">Usuario</div>
-                  </div>
+        </div>
+        <div class="frame-57">
+          <div class="group-5">
+            <div class="dropdown-desktop-usuario">
+              <div class="frame-427319577">
+                <div class="frame-52">
+                  <div class="usuario">Usuario</div>
                 </div>
-                <div class="desplaguebaleuser">
-                  <div class="botonuser">
-                    <div class="wrapper">
-                      <div class="text">
-                        <div class="placeholder">Usuario</div>
-                        <img class="vector" src="vector0.svg" />
-                      </div>
+              </div>
+              <div class="dropdown-desktop">
+                <div class="boton">
+                  <div class="wrapper">
+                    <div class="text">
+                      <div class="placeholder">Usuario</div>
+                      <img class="vector" src={flecha} />
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="divpassword">
-                <div class="password">
-                  <div class="passwordletras">
-                    <div class="password">Contraseña</div>
-                  </div>
-                </div>
-                <div class="registropassword">
-                  <div class="ejemplodiv">
-                    <div class="ejemplo">Ejemplo</div>
-                  </div>
-                </div>
-                <div class="frame-427319578">
-            <div class="has-olvidado-tu-contrase-a">
-              ¿Has olvidado tu contraseña?
             </div>
-          </div>
+            <div class="contrase-a">
+              <div class="frame-427319577">
+                <div class="frame-52">
+                  <div class="contrase-a2">Contraseña</div>
+                </div>
               </div>
-              <div class="loginboton">
-                <div class="login">Login</div>
+              <div class="frame-53">
+                <div class="frame-58">
+                  <div class="ejemplo">Ejemplo</div>
+                </div>
               </div>
+              <div class="frame-427319578">
+                <div class="has-olvidado-tu-contrase-a">
+                  ¿Has olvidado tu contraseña?
+                </div>
+              </div>
+            </div>
+            <div class="boton2">
+              <div class="login">Iniciar sesión</div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
+
 
   )
 }
